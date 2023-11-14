71be4dbc89d5368559ae2d86b517b544c7169666
krishna P <54264561+krishna9901@users.noreply.github.com>
2023-11-14 19:49:11 +0530

    Create footer.php

---
+++ application/views/includes/footer.php
@@ -0,0 +1,29 @@
+
+
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>user management</b> Admin System | Version 1.6
+        </div>
+        <strong>Copyright &copy; 2021-2023 <a href="<?php echo base_url(); ?>">krishna</a>.</strong> All rights reserved.
+    </footer>
+    
+    <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/dist/js/adminlte.min.js" type="text/javascript"></script>
+    <!-- <script src="<?php echo base_url(); ?>assets/dist/js/pages/dashboard.js" type="text/javascript"></script> -->
+    <script src="<?php echo base_url(); ?>assets/js/jquery.validate.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/validation.js" type="text/javascript"></script>
+
+
+    <script src="<?php echo base_url(); ?>assets/dist/js/custom.js"type="text/javascript"></script>
+    <script type="text/javascript">
+        var windowURL = window.location.href;
+        pageURL = windowURL.substring(0, windowURL.lastIndexOf('/'));
+        var x= $('a[href="'+pageURL+'"]');
+            x.addClass('active');
+            x.parent().addClass('active');
+        var y= $('a[href="'+windowURL+'"]');
+            y.addClass('active');
+            y.parent().addClass('active');
+    </script>
+  </body>
+</html>
